c9917e692b54c0dfc8c0e22e844f740f2dd804fe
unknown <C17764691@mydit.ie>
2019-03-20 00:53:21 +0000

    added designer door mat problem

---
+++ DesignerDoorMat.py
@@ -0,0 +1,30 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# -> Mat size must be NxM. (N is an odd natural number, and M is 3 times N.)
+# -> The design should have 'WELCOME' written in the center.
+# -> The design pattern should only use |, . and - characters.
+
+N, M = map(int,input().split())
+
+for i in range(int(N/2)):
+    for j in range(int((M / 2) - 1) - (i*3)):
+        print("-", end='')
+    print(".|." * ((i * 2) + 1), end='')
+
+    for j in range(int((M / 2) - 1) - (i*3)):
+        print("-", end='')
+    print()
+
+print("-" * (int((M - 7)/2)), end='')
+print("WELCOME", end='')
+print("-" * (int((M - 7)/2)), end='')
+print()
+
+for i in reversed(range(int(N/2))):
+    for j in range(int((M / 2) - 1) - (i*3)):
+        print("-", end='')
+    print(".|." * ((i * 2) + 1), end='')
+
+    for j in range(int((M / 2) - 1) - (i*3)):
+        print("-", end='')
+    print()
